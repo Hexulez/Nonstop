b9d8897cc03518839b4640fd1886ccd35fd751f0
Hexulez <henry.juhola@edu.keuda.fi>
2021-11-30 02:03:08 +0200

    Valmis arvauspeli

---
+++ Temp/split_text.js
@@ -0,0 +1,23 @@
+function jako() {
+  let jono = '<a href="https://www.google.com/logos/2010/pacman10-i.html">suprise</a>';
+  let egg = "";
+  let geg ="";
+  let leg ="";
+  let beg ="";
+  for (var i = 0; i <= jono.length; i++) {
+    egg += jono.charAt(i)
+    i++
+    geg +=jono.charAt(i)
+    i++
+    leg +=jono.charAt(i)
+    i++
+    beg +=jono.charAt(i)
+    console.log(jono);
+    console.log(egg);
+    console.log(geg);
+    console.log(leg);
+    console.log(beg);
+
+  }
+
+}
